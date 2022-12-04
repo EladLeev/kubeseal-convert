f4c1ac1f5cd8f4091b51d8dde82ea31ac0c1cd27
Elad Leev <eladleev@gmail.com>
2022-12-04 16:18:47 +0000

    add some tests

---
+++ pkg/kubeseal-convert/handlers/secretsmanager/secretsmanager_test.go
@@ -0,0 +1,29 @@
+package secretsmanager
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+func Test_getSecret(t *testing.T) {
+	type args struct {
+		svc        *secretsmanager.Client
+		secretName string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getSecret(tt.args.svc, tt.args.secretName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getSecret() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
