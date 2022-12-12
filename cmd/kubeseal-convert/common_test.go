96e6029c2550575d72da849abc68359fa94b6dab
Pavel Durov <pav3ldurov@gmail.com>
2022-12-12 09:23:16 +0000

    chore: vault cmd tests (#13)

---
+++ cmd/kubeseal-convert/common_test.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func ExecuteCommand(root *cobra.Command, args ...string) (output string, err error) {
+	buf := new(bytes.Buffer)
+	root.SetOut(buf)
+	root.SetErr(buf)
+	root.SetArgs(args)
+
+	err = root.Execute()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return buf.String(), err
+}
