9bb668200863122e0aadf572d26dc302b703ac48
Elad Leev <eladleev@gmail.com>
2024-08-24 12:31:43 +0100

    fix tests

--- cmd/kubeseal-convert/azurekeyvault_test.go
+++ cmd/kubeseal-convert/azurekeyvault_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/mock"
-
 	"gotest.tools/assert"
 
 	"github.com/eladleev/kubeseal-convert/mocks"
@@ -15,17 +14,22 @@ func TestAzureKeyVaultCmd(t *testing.T) {
 
 	// mock kubeseal
 	mockKubeSeal := mocks.NewKubeSeal(t)
-	mockKubeSeal.On("BuildSecretFile", mock.Anything, mock.AnythingOfType("bool")).Return()
+	mockKubeSeal.On("BuildSecretFile", mock.AnythingOfType("domain.SecretValues"), mock.AnythingOfType("bool")).Return()
 	KubeSeal = mockKubeSeal
 
 	// mock azurekeyvault
 	mockAzureKeyVault := mocks.NewAzureKeyVault(t)
 	mockAzureKeyVault.On("GetSecrets", mock.Anything).Return(map[string]interface{}{
 		"key": "value",
-	})
+	}, nil)
 	AzureKeyVault = mockAzureKeyVault
 
 	// test az command
-	output, _ := ExecuteCommand(rootCmd, "az", "dev/secret", "--name", "blabla")
+	output, err := ExecuteCommand(rootCmd, "az", "dev/secret", "--name", "blabla")
+	assert.NilError(t, err)
 	assert.Equal(t, "", output)
+
+	// Verify that the mocks were called as expected
+	mockAzureKeyVault.AssertExpectations(t)
+	mockKubeSeal.AssertExpectations(t)
 }
--- cmd/kubeseal-convert/vault_test.go
+++ cmd/kubeseal-convert/vault_test.go
@@ -10,21 +10,26 @@ import (
 )
 
 func TestVaultCmd(t *testing.T) {
-	rootCmd.AddCommand(secretsmanagerCmd)
+	rootCmd.AddCommand(vaultCmd)
 
 	// mock kubeseal
 	mockKubeSeal := mocks.NewKubeSeal(t)
-	mockKubeSeal.On("BuildSecretFile", mock.Anything, mock.AnythingOfType("bool")).Return()
+	mockKubeSeal.On("BuildSecretFile", mock.AnythingOfType("domain.SecretValues"), mock.AnythingOfType("bool")).Return()
 	KubeSeal = mockKubeSeal
 
 	// mock vault
 	mockVault := mocks.NewVault(t)
 	mockVault.On("GetSecret", mock.Anything).Return(map[string]interface{}{
 		"key": "value",
-	})
+	}, nil)
 	Vault = mockVault
 
 	// test vault command
-	output, _ := ExecuteCommand(rootCmd, "vault", "dev/secret", "--name", "blabla")
+	output, err := ExecuteCommand(rootCmd, "vault", "dev/secret", "--name", "blabla")
+	assert.NilError(t, err)
 	assert.Equal(t, "", output)
+
+	// Verify that the mocks were called as expected
+	mockVault.AssertExpectations(t)
+	mockKubeSeal.AssertExpectations(t)
 }
